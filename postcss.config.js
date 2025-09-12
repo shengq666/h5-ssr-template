894a98b75f4e1f0f15c40e55324cf8eb76539959
shengq <shengq007@outlook.com>
2025-09-12 16:42:34 +0800

    feat: postcss 配置不转换第三方UI 库

--- postcss.config.js
+++ postcss.config.js
@@ -10,7 +10,23 @@ module.exports = {
       propList: ['*'], // 指定哪些属性需要转换，['*'] 表示所有属性
       viewportUnit: 'vw', // 转换后的视口单位
       fontViewportUnit: 'vw', // 字体转换后的视口单位
-      selectorBlackList: [], // 指定不转换的选择器，可以是字符串或正则表达式
+      selectorBlackList: [
+        // 忽略包含特定类名的选择器
+        '.adm-', // 排除所有 antd-mobile 组件 (类名以 adm- 开头)
+        '.ant-', // 如果有使用 antd，也一并排除
+        'ignore', // 忽略包含 'ignore' 的类
+        'hairlines', // 忽略细边框处理相关类
+        // 匹配 :global(.am-xxx) 或 :global .am-xxx
+        /:global\(\.am-/,
+        /:global\s+\.am-/,
+
+        // 匹配 ::v-deep(.am-xxx) 或 :deep(.am-xxx)（兼容 deep 写法）
+        /::v-deep\(\.am-/,
+        /:deep\(\.am-/,
+
+        // 如果有其他第三方库前缀（如 .mt- 代表 mint-ui），可补充
+        // /:global\(\.mt-/,
+      ],
       minPixelValue: 1, // 最小的转换像素值，小于等于此值的 px 不转换
       mediaQuery: false, // 是否允许在媒体查询中转换 px
       replace: true, // 是否直接替换值而不添加备用属性
