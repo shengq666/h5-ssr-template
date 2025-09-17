fea2ac865705d23ae216ba8824e95ed69b9d76c7
shengq <shengq007@outlook.com>
2025-09-17 16:00:22 +0800

    feat: splitChunks

--- next.config.ts
+++ next.config.ts
@@ -11,48 +11,51 @@ const nextConfig: NextConfig = {
   },
   // 压缩配置
   compress: true, // 默认情况下，当使用 next start 或自定义服务器时，Next.js 会使用 gzip 压缩渲染内容和静态文件。这是针对未配置压缩的应用程序的优化。如果您的应用程序已通过自定义服务器配置了压缩，Next.js 将不会重复添加压缩功能。
-  webpack: (config, { dev, isServer }) => {
-    if (!isServer && !dev) {
-      // 只在生产环境的客户端构建中应用这些优化
-      config.optimization = {
-        ...config.optimization,
-        splitChunks: {
-          chunks: 'all',
-          // minSize: 20000, // 生成 chunk 的最小体积（20KB）
-          maxSize: 1024 * 1024 * 5, // 生成 chunk 的最大体积（0表示不限制）
-          minChunks: 1, // 模块被引用的最小次数
-          // maxAsyncRequests: 30, // 按需加载时的最大并行请求数
-          // maxInitialRequests: 30, // 入口点的最大并行请求数
-          cacheGroups: {
-            // 第三方库分包
-            vendor: {
-              test: /[\\/]node_modules[\\/]/,
-              name: 'vendors',
-              chunks: 'all',
-              priority: 10,
-            },
-            // 公共代码分包
-            common: {
-              name: 'common',
-              minChunks: 2, // 至少被引用2次
-              chunks: 'all',
-              priority: 5,
-            },
-            // 框架代码分包
-            framework: {
-              test: /[\\/]node_modules[\\/](react|react-dom|next)[\\/]/,
-              name: 'framework',
-              chunks: 'all',
-              priority: 20,
-            },
-          },
-        },
-        runtimeChunk: 'single',
-      }
-    }
+  //Next.js 默认会为每个 app/ 或 pages/ 目录下的路由页面生成独立的 JavaScript 包。
+  // 用户访问某个页面时，通常只会下载该页面相关的代码。
+  // 理论上来说页面代码体积很少会特别大，不需要自己 splitChunks。需要的往往是第三方库或大体积共用依赖
+  // webpack: (config, { dev, isServer }) => {
+  //   if (!isServer && !dev) {
+  //     // 只在生产环境的客户端构建中应用这些优化
+  //     config.optimization = {
+  //       ...config.optimization,
+  //       splitChunks: {
+  //         chunks: 'all',
+  //         // minSize: 20000, // 生成 chunk 的最小体积（20KB）
+  //         maxSize: 1024 * 1024 * 5, // 生成 chunk 的最大体积（0表示不限制）
+  //         minChunks: 1, // 模块被引用的最小次数
+  //         // maxAsyncRequests: 30, // 按需加载时的最大并行请求数
+  //         // maxInitialRequests: 30, // 入口点的最大并行请求数
+  //         cacheGroups: {
+  //           // 第三方库分包
+  //           vendor: {
+  //             test: /[\\/]node_modules[\\/]/,
+  //             name: 'vendors',
+  //             chunks: 'all',
+  //             priority: 10,
+  //           },
+  //           // 公共代码分包
+  //           common: {
+  //             name: 'common',
+  //             minChunks: 2, // 至少被引用2次
+  //             chunks: 'all',
+  //             priority: 5,
+  //           },
+  //           // 框架代码分包
+  //           framework: {
+  //             test: /[\\/]node_modules[\\/](react|react-dom|next)[\\/]/,
+  //             name: 'framework',
+  //             chunks: 'all',
+  //             priority: 20,
+  //           },
+  //         },
+  //       },
+  //       runtimeChunk: 'single',
+  //     }
+  //   }
 
-    return config
-  },
+  //   return config
+  // },
   // 图片域名白名单
   images: {
     domains: ['cdn.yourdomain.com'],
